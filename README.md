f64480d9149d2e9b1f09390bbc4c70acac182978
dragonero2704 <roberto.rudi04@gmail.com>
2024-10-01 17:01:44 +0200

    init

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# obsidian-utils
-A repository for dataviews and templates for obsidian editor
+# Obsidian-utils
+
+A repository for dataviews and templates for [obsidian](https://obsidian.md/)
+
+To use this repository, clone it in the root of your obsidian note vault.
+
+Dataview plugin documentation [link](https://blacksmithgu.github.io/obsidian-dataview/)
---
+++ views/index.js
@@ -0,0 +1,41 @@
+/**
+ * This dataviewjs file generates a index with headers
+ * for every file in your vault
+ * It's possibile to exclude files by populating the excluded array
+ * 
+ * const excluded = ["filenameToExclude", "anotherFilename", ...]
+ * 
+ * Dataviewjs docs https://blacksmithgu.github.io/obsidian-dataview/
+ */
+const defaultExcluded = ["README", "Note", "TODO"]
+const defaultSortFunction = (page) => page.file.name
+const excluded = this.input ? this.input.exclude : defaultExcluded 
+const sortFunction = this.input ? this.input.sort : defaultSortFunction
+const pages = dv.pages()
+.filter(page=>!(excluded.includes(page.file.name) || page.file.path === dv.current().file.path))
+.sort(sortFunction)
+
+const markdown = await Promise.all(pages.map(async page=>{
+	console.log(page.file.sequence)
+	const filename = page.file.name
+	const link = page.file.link
+	const contents = await dv.io.load(page.file.path)
+	const rows = contents.split("\n").filter(row=>row.trim().length!==0)
+	const headingsRegex = "^(\#{1,6}\\s\\[?(([^\\]]*)\\]{0,2}?).*)"
+	const voices = rows.map(row=>{
+		const matches = row.match(headingsRegex)
+		if(matches !== null)
+		{
+			const count = (matches[1].match(/\#/g) ?? []).length - 1
+			const indexVoice = `[[${filename}#${matches[1]}|${matches[2]}]]`
+			const display = `${"  ".repeat(count)}- ${"#".repeat(count+1)} ${indexVoice}`
+			return display
+		}
+		return null
+	}).filter(row=>row!==null)
+	const linkCount = voices.length
+	const res = `# ${link} (${linkCount})\n${voices.join("\n")}`
+	return res
+}))
+
+dv.paragraph(markdown.join("\n---\n"))
